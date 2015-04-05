b8a26ba0a2fc8b1132d2bfee6b6208e1984b4906
Paul <paulbrodner@gmail.com>
2015-04-05 18:04:15 +0200

    Added Vagrant file

--- configs/vagrant.rb
+++ configs/vagrant.rb
@@ -6,7 +6,8 @@ module Vagrant
       hostname "127.0.0.1"
       username "vagrant"
       port 2222
-      key_data "configs/vagrant/private_key"
+      # run vagrant ssh-config to find the path of key_data
+      key_data "configs/vagrant/.vagrant/machines/default/virtualbox/private_key"
     end
 
     tmp "/tmp"
--- scripts/install.rb
+++ scripts/install.rb
@@ -2,7 +2,8 @@ module Install
   include DevOn
   require File.expand_path("configs/#{ENV['config']}")
   config = DevOn::Config.send(ENV['config'])
-  Command.run_shell(config, "ls -la /vagrant")
+     
+  Command.run_shell(config, "cd /vagrant/ && chmod +x alfresco-community-5.0.d-installer-linux-x64.bin")
 
   provision_on config
 end
