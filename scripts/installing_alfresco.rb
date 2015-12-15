cc02bf44165f76d9f084cd04a33c76a0a5e083f5
Paul Brodner <paulbrodner@gmail.com>
2015-12-15 19:16:21 +0200

    update installer script

--- scripts/installing_alfresco.rb
+++ scripts/installing_alfresco.rb
@@ -2,26 +2,18 @@
 module Installing_alfresco
   include DevOn
 
-  ALFRESCO_HOME = "alfresco-51-b372"
-  INSTALLER_PATH = "/data/nfs/software/alfresco/alfresco-5.1/build-373/alfresco-enterprise-installer-20151006-SNAPSHOT-372-linux-x64.bin"  
-  
-  @enable_components  = "'javaalfresco libreofficecomponent alfrescosolr alfrescosolr4 alfrescowcmqs alfrescogoogledocs'"
-  @disable_components = "'postgres'"
-  @db_name="alfresco"
-  @jdbc_username="alfresco"
-  @jdbc_password="admin"
-  @jdbc_url=$config.installer.jdbc_url
-  @driver="org.postgresql.Driver"
-  @install_as_a_service="0"
+  ALFRESCO_HOME = "alfresco-one-b577"
+  INSTALLER_PATH = "/data/nfs/software/alfresco/alfresco-5.1/build-577/alfresco-one-installer-20151210-SNAPSHOT-577-linux-x64.bin"
 
-  unattended = %Q{
-cd #{File.dirname(INSTALLER_PATH)} && #{INSTALLER_PATH} --mode unattended
---alfresco_admin_password admin --disable-components postgres --prefix /home/#{ENV['username']/alfresco-51-b372
- #{ALFRESCO_HOME}}.gsub!("\n", " ")
-
-  #Command.run_shell("chmod +x #{INSTALLER_PATH}")
+  unattended = %Q{cd #{File.dirname(INSTALLER_PATH)} && ./#{File.basename(INSTALLER_PATH)}
+ --unattendedmodeui none --mode unattended
+ --alfresco_admin_password admin
+ --disable-components postgres
+ --prefix ~/#{ALFRESCO_HOME}
+ --jdbc_username admin
+ --jdbc_password admin
+ --baseunixservice_install_as_service 0}.gsub("\n", "")
   Command.run_shell(unattended)
 
   provision_on $config
 end
-  
